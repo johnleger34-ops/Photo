8e25fc6c009b70fdbab0a8b67f24379873023344
johnleger34-ops <johnleger34@gmail.com>
2026-08-16 05:26:07 +0000

    Fix missing Compose setContent import

--- android/app/src/main/java/com/nolimits/photoeditor/MainActivity.kt
+++ android/app/src/main/java/com/nolimits/photoeditor/MainActivity.kt
@@ -9,6 +9,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
